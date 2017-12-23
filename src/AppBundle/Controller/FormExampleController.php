2046c7ccb8ec3c5966cf0383faeec545bfb1796c
Shaun Thornburgh <personal@shaunthornburgh.com>
2017-12-23 17:03:04 +0000

    Create produc form

--- src/AppBundle/Controller/FormExampleController.php
+++ src/AppBundle/Controller/FormExampleController.php
@@ -5,51 +5,34 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AppBundle\Form\Type\ProductType;
+use AppBundle\Entity\Product;
 
 class FormExampleController extends Controller
 {
-    /**
-     * @Route("/", name="form_example")
-     */
-    public function formExampleAction(Request $request)
+  /**
+   * @Route("/", name="form_example")
+   */
+  public function formExampleAction(Request $request)
+  {
+    $form = $this->createForm(ProductType::class);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid())
     {
-        $form = $this->createFormBuilder()
-          ->add('personName', TextType::class)
-          ->add('submit', SubmitType::class, [
-            'label' => 'Submit me now!',
-            'attr' => [
-              'class' => 'btn btn-success'
-            ]
-          ])
-          ->getForm()
-        ;
+      $em = $this->getDoctrine()->getManager();
 
-        $form->handleRequest($request);
+      $product = $form->getData();
 
-        if ($form->isSubmitted() && $form->isValid()) {
+      $em->persist($product);
+      $em->flush();
 
-          $name = $form->getData()['personName'];
-
-          dump($name);
-
-          $this->sendMail($name);
-        }
-
-        return $this->render('form-example/index.html.twig', [
-          'myForm' => $form->createView()
-        ]);
+      $this->addFlash('success', 'We saved the product with id: ' . $product->getId());
     }
 
-    private function sendMail($personName)
-    {
-      $mail = (new \Swift_Message('This is the subject'))
-        ->setFrom('me@form_demo.test')
-        ->setTo('personal@shaunthornburgh.com')
-        ->setBody($personName)
-      ;
-
-      $this->get('mailer')->send($mail);
-    }
+    return $this->render('form-example/index.html.twig', [
+      'form' => $form->createView()
+    ]);
+  }
 }
